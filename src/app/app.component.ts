8805eea13fcefab3ccea4131a8703338bef0cc70
Verda Muso <gearmobile@gmail.com>
2023-11-30 22:49:45 +0300

    add taiga-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'tg-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
 })
 export class AppComponent {
   title = 'taiga-ui';
