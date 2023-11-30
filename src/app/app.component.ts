cfda5f2449110d7b53a962cd218df48d1e720e5b
Verda Muso <gearmobile@gmail.com>
2023-11-30 23:24:43 +0300

    add taiga-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { TuiInputModule } from '@taiga-ui/kit';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'tg-root',
   standalone: true,
-  imports: [CommonModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
+  imports: [CommonModule, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiInputModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './app.component.scss',
     providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
 })
 export class AppComponent {
-  title = 'taiga-ui';
+  public readonly title = 'taiga-ui';
+  public readonly testForm = new FormGroup({
+    testValue: new FormControl('mail@mail.ru'),
+  });
 }
